5b48e480937f9ca2ecf6f42b5d69f1b8bdda1b37
Yujan3333 <yujanbasnet18@gmail.com>
2022-07-29 09:21:36 +0545

    Lesson63KeyboardEvents

--- 59-FormEvents/form-script.js
+++ 59-FormEvents/form-script.js
@@ -1,13 +1,16 @@
 const form= document.querySelector(".form");
 // const username=document.querySelector("#username");
 const feedback=document.querySelector(".feedback");
+
+const usernamePattern= /^[a-zA-Z]{6,10}$/;
+
 form.addEventListener('submit',e=>
 {
     e.preventDefault();       //prevents the default submit action of browser is refresh the page
     // console.log(username.value); //gets the value that is entered in the username input
     // console.log(form.username.value); //another way to input the username
     const username=form.username.value;
-    const usernamePattern= /^[a-zA-Z]{6,10}%/;
+    // const usernamePattern= /^[a-zA-Z]{6,10}%/; //Commenting out to make it globally accessible for the live action below too
 
     if (usernamePattern.test(username)) {
         //positive info
@@ -28,3 +31,13 @@ form.addEventListener('submit',e=>
 // let result=username.search(pattern);    
 // console.log(result);    //first match in the pattern position if not matched then -1
 
+//live testing the username
+form.username.addEventListener('keypress',e =>{
+    if(usernamePattern.test(e.target.value)){
+        console.log("passed");
+        form.username.setAttribute("class","ok");
+    }else{
+        console.log("fail");
+        form.username.setAttribute("class","fail");
+    }
+})
