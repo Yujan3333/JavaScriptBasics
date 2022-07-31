c4dda76ab250a6ff875e3039fe22bece9752f2c3
Yujan3333 <yujanbasnet18@gmail.com>
2022-07-31 20:01:21 +0545

    Lesson83Date()

---
+++ 82-Dates/script.js
@@ -0,0 +1,50 @@
+/* Basics notation in Date Object
+const now = new Date();
+
+console.log(now);
+
+// year month and day
+now.getFullYear();
+now.getMonth();
+now.getDate();
+now.getDay();
+now.getHours();
+now.getMinutes();
+now.getSeconds();
+
+//timeouts
+now.getTime();
+
+//date strings
+now.toDateString();
+now.toTimeString();
+now.toLocaleString();
+*/
+
+// --------------------------------------
+
+/*Lesson83:Timestamps and Comparisons 
+//Directly assigining the date
+const before=new Date('January 25 2021');
+const now= new Date();
+
+// console.log(before);
+// console.log(now);
+
+const diff=now.getTime()-before.getTime();
+// the diff here is in milliseconds
+console.log(diff);
+
+//1000milliseconds = 1 second
+const seconds=Math.round(diff/1000);
+console.log("Seconds:",seconds);
+
+const minutes=Math.round(seconds/60);
+console.log("Minutes:",minutes);
+
+const hours=Math.round(seconds/60/60);
+console.log("Hours:",hours);
+
+const days=Math.round(hours/24);
+console.log("Last posted Days Count",days);
+*/
